bd54dd9b39d730956549c9050dc5fe034ab59265
Zachary Freed <zfreed125@gmail.com>
2022-06-15 13:41:47 -0500

    refactor(react lesson): styling app page

--- src/componants/Navbar.js
+++ src/componants/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
+import logo from "../images/react_logo.png";
+import "../style.css"
 
 export default function Nav() {
     return (
-        <h1>Navbar goes Here</h1>
+        <nav>
+        <img src={logo} alt="logo" className="nav--icon" />
+        <h3 className="nav--logo_text">React Facts</h3>
+        <h4 className="nav--title">React Course-Project 1</h4>
+        </nav>
     )
-}
+} 

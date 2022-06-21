82dbe244b199dab04821ccb41534c8ac802d7d87
Zachary Freed <zfreed125@gmail.com>
2022-06-21 11:12:47 -0500

    refactor(Final): added link to wookiepedia, ok now im done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Insta from "../assets/Instagram_icon.png"
 import Twitter from "../assets/Twitter.png"
+import Jesse from "../assets/wookiepedia.png"
 
 const instagramurl = 'https://www.instagram.com/';
 export default function Footer() {
@@ -11,6 +12,9 @@ export default function Footer() {
             </a>
             <a href='https://twitter.com/ZachFreed2'>
                 <img src={Twitter} width="40px" alt="twitter" className="footer--twitter" />
+            </a>
+            <a href='https://starwars.fandom.com/wiki/CT-5597'>
+                <img src={Jesse} width="40px" alt="jesse" className="footer--jesse" />
             </a>
         </footer>
     )

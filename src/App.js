f1d989457bfbb8a4ed43be43c3cf44227d3bd4a8
Zachary Freed <zfreed125@gmail.com>
2022-06-27 15:25:47 -0500

    refactor(Card): passing props

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import "./style.css"
+// import Katie from "./assets/katee_zaferes.png"
+
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <Card 
+                // img={Katie}
+                img="katee_zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
 import React from "react";
-import Katie from "../assets/katee_zaferes.png"
 import Star from "../assets/star.png"
 
-export default function Card() {
+export default function Card(props) {
+    // console.log(props)
     return (
         <div className="card">
-            <img src={Katie} alt="katie" className="card--photo" />
+            <img src={`../assets/${props.img}`} alt={props.img}  className="card--photo" />
             <div className="card--stats">
                 <img src={Star} alt="star" className="card--star" />
-                <span>5.0</span>
-                <span className="grey">(6) - </span>
-                <span className="grey">USA</span>
+                <span>{props.rating}</span>
+                <span className="grey">({props.reviewCount}) - </span>
+                <span className="grey">{props.country}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
